7f7c69918322a4e2fd72fa337eea6b5b4a657ea2
VDoring <hojun.dev@gmail.com>
2023-01-19 03:19:08 +0900

    달력 스케줄 내용입력창 변경
    sweetalert2

--- src/main/java/com/example/unplugged/controller/UserController.java
+++ src/main/java/com/example/unplugged/controller/UserController.java
@@ -86,18 +86,6 @@ public class UserController {
         return "user/event";
     }
 
-    @GetMapping("/user/event/eventadd")
-    public String dispEventWrite(@AuthenticationPrincipal MemberUser user, Model model) {
-        /* 임시로 해제했어요 - 호준
-        if (user == null) {
-            return "redirect:/login";
-        }
-        */
-
-        model.addAttribute("user", user);
-        return "user/eventadd";
-    }
-
     @GetMapping("/user/notice")
     public String dispNotice(@AuthenticationPrincipal MemberUser user, Model model) {
 
---
+++ src/main/resources/static/js/event.js
@@ -0,0 +1,5 @@
+
+
+function testPrint() {
+
+}
