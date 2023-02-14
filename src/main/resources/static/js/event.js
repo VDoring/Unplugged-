79023ba3cc6332fd82a06c8e87d8d9bdde548b0e
VDoring <hojun.dev@gmail.com>
2023-02-14 17:32:10 +0900

    스케줄 입력 시 날짜, 시간 범위 설정이 되도록 시도

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -1,5 +1,18 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
 
-
-function testPrint() {
-
+if (dd < 10) {
+    dd = '0' + dd;
 }
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+today = yyyy + '-' + mm + '-' + dd;
+
+// 작동하지 않는 상태
+document.getElementById("schedule_date_start").setAttribute("value", today);
+document.getElementById("schedule_date_end").setAttribute("value", today);
